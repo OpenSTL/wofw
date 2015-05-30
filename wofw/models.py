af9360da5cc2666d049d7fdcf321a55894dc7e32
eleanortutt <eleanor.tutt@gmail.com>
2015-05-30 16:28:00 -0500

    Update models.py

--- wofw/models.py
+++ wofw/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class ward(models.Model):
+    wardnumber = models.IntegerField(default=0)
+    
+class alderman(models.Model):
+    ward = models.ForeignKey(ward)
+    alderlast = models.CharField(max_length=100)
+    alderfull = models.CharField(max_length=200)
+  
+class bill(models.Model):
+    billnumber = models.IntegerField(default=0)
+    billname = models.CharField(max_length=200)
+    
+class vote(models.Model):
+    AYE = 'aye'
+    NAY = 'nay'
+    PRESENT = 'present'
+    NOVOTE = 'no vote'
+    VOTE_CHOICES = (
+        (AYE, 'aye'),
+        (NAY, 'nay'),
+        (PRESENT, 'present'),
+        (NOVOTE, 'no vote'),
+    )
+    COMMITTEE = 'committee'
+    PERFECTION = 'perfection'
+    FINAL = 'final'
+    READING_CHOICES = (
+        (COMMITTEE, 'committee'),
+        (PERFECTION, 'perfection'),
+        (FINAL, 'final'),
+    )
+    alderlast = models.ForeignKey(alderman)
+    billnumber = models.ForeignKey(billnumber)
+    vote = models.CharField(max_length=10, choices=VOTE_CHOICES)
+    reading = models.CharField(max_length=20, choices=READING_CHOICES) 
+    date = models.DateField()
+    
+    
